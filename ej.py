c80098f23799996a5185a1555a422756b28fd990
jhonazt <jhonzatre2011@gmail.com>
2016-12-01 11:12:57 -0500

    Pantalla

--- ej.py
+++ ej.py
@@ -1,6 +1,15 @@
 import sys
 import wx
 
+#!/usr/bin/python
+# simple.py
+import wx
+app = wx.App()
+frame = wx.Frame(None, -1, 'simple.py')
+frame.Show()
+app.MainLoop()
+
+
 print "Hola"
 
 """git clone https://github.com/JjVera96/ProyectoIO.git
