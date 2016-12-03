6896a208266ac078d869d9587e6b8fa73948f9a0
jhonazt <jhonzatre2011@gmail.com>
2016-12-03 15:44:33 -0500

    botones

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ class ventanaInicio(wx.Frame):
 		self.SetTitle('Investigacion de Operaciones')
 		self.SetSize((600,600))
 		Titulo = wx.StaticText(panel, -1, "Investigacion De Operaciones", (190,10))
+		
+		sz=wx.BoxSizer(wx.VERTICAL)
+		boton1 = wx.Button(panel,-1, u"Dos Fases")
+		boton2 = wx.Button(panel,-1, u"Lagrange")
+		boton3 = wx.Button(panel,-1, u"Salir")
+
+		
+		sz.Add(boton1,1,wx.EXPAND|wx.ALL,40)
+		sz.Add(boton2,1,wx.EXPAND|wx.ALL,40)
+		sz.Add(boton3,1,wx.EXPAND|wx.ALL,60)
+
+		self.SetSizer(sz)
 		self.Centre()
 		self.Show(True)
 
