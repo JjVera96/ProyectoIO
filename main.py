592dfc614198535f64838f1567e6876447334e4a
JjVera96 <jjvera96@gmail.com>
2016-12-05 18:54:33 -0500

    FuncionamientoBotones

--- main.py
+++ main.py
@@ -2,13 +2,55 @@
 import wx
 import sys
 
-class NumeroVariables(wx.Frame):
+class DosFasesUI(wx.Frame):
     def __init__ (self, *args, **kwargs):
-        super(NumeroVariables, self).__init__(*args, **kwargs)
+        super(DosFasesUI, self).__init__(*args, **kwargs)
         self.InitUI()
 
     def InitUI(self):
-        self.SetTitle('Investigacion de Operaciones')
+        self.SetTitle('Metodo Dos Fases')
+        self.SetSize((400,400))
+        Titulo = wx.StaticText(self, -1, "Investigacion De Operaciones", (190,10))
+
+        p1=wx.Panel(self)
+        p2=wx.Panel(self)
+        
+        mainsz = wx.BoxSizer(wx.VERTICAL)
+        p1sz = wx.BoxSizer(wx.VERTICAL)
+        p2sz = wx.BoxSizer(wx.VERTICAL)
+
+
+        st1=wx.StaticText(p1,-1,"Inserte El Numero De Variables",(0,10))
+        txt1=wx.TextCtrl(p1,-1)
+        st2=wx.StaticText(p2,-1,"Inserte El Numero De Restricciones")
+        txt2=wx.TextCtrl(p2,-1)
+        bt=wx.Button(self,-1,"Continuar")
+
+        
+
+        p1sz.Add(st1,1,wx.EXPAND|wx.ALL,10)
+        p1sz.Add(txt1,1,wx.EXPAND|wx.ALL,10)
+        p2sz.Add(st2,1,wx.EXPAND|wx.ALL,10)
+        p2sz.Add(txt2,1,wx.EXPAND|wx.ALL,10)
+        mainsz.Add(p1,1,wx.EXPAND)
+        mainsz.Add(p2,1,wx.EXPAND)
+        mainsz.Add(bt,1,wx.ALIGN_CENTRE|wx.ALL,30)
+
+        p1.SetSizer(p1sz)
+        p2.SetSizer(p2sz)
+
+        self.SetSizer(mainsz)
+        self.Centre()
+        self.Show(True)
+
+
+class LagrangeUI(wx.Frame):
+    def __init__ (self, *args, **kwargs):
+        super(LagrangeUI, self).__init__(*args, **kwargs)
+        self.InitUI()
+
+    def InitUI(self):
+        self.SetTitle('Algoritmo de Lagrange')
         self.SetSize((400,400))
         Titulo = wx.StaticText(self, -1, "Investigacion De Operaciones", (190,10))
 
@@ -40,13 +82,10 @@ class NumeroVariables(wx.Frame):
 
         p1.SetSizer(p1sz)
         p2.SetSizer(p2sz)
-        
-
 
         self.SetSizer(mainsz)
         self.Centre()
         self.Show(True)
-
 
 class ventanaInicio(wx.Frame):
     def __init__ (self, *args, **kwargs):
@@ -71,16 +110,20 @@ class ventanaInicio(wx.Frame):
 
         self.Bind(wx.EVT_BUTTON, self.onClickButton1,boton1)
         self.Bind(wx.EVT_BUTTON, self.onClickButton2,boton2)
+        self.Bind(wx.EVT_BUTTON, self.onQuit, boton3)
 
         self.SetSizer(sz)
         self.Centre()
         self.Show(True)
 
     def  onClickButton1(self,event):
-         NumeroVariables(None)
+        DosFasesUI(None)
        
     def onClickButton2(self,event):
-        print "Lagrange"
+        LagrangeUI(None)
+
+    def onQuit(self,event):
+        self.Destroy()
 
 
 
