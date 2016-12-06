9fe6ba2481faf948b187886b69c0e739f413adfc
JjVera96 <jjvera96@gmail.com>
2016-12-05 22:34:33 -0500

    MitadProyecto

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 #-*- coding: utf-8 -*-
 import wx
 import sys
+import wx.grid
 
 cantVar = 0
 cantRes = 0
+matriz = []
 
-class IntermedioUI(wx.Frame):
-    
+def PrimeraParte():
+    pass
+
+def SegundaParte():
+    pass
+
+class RestriccionesUI(wx.Frame):   
     def __init__ (self, *args, **kwargs):       
-        super(IntermedioUI, self).__init__(*args, **kwargs)
+        super(RestriccionesUI, self).__init__(*args, **kwargs)
         self.InitUI()
 
     def InitUI(self):
-        self.SetTitle('Pidiendo')
-        self.SetSize((400,400))
-        print cantVar, cantRes
+        self.SetTitle('Variables')
+        self.SetSize((500, 1000))
+        self.panel = wx.Panel(self)
+        self.tabla = wx.grid.Grid(self.panel)
+        self.tabla.CreateGrid(cantVar+1, cantRes+1)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.tabla)
+        self.panel.SetSizer(self.sizer)
+        self.bt=wx.Button(self.panel,-1,"Continuar", pos = (220, 500))
+        self.Bind(wx.EVT_BUTTON, self.onContinuar, self.bt)
         self.Centre()
         self.Show(True)
 
+    def onContinuar(self, event):
+        global matriz
+        self.tablero = []
+        self.linea = []
+        for i in range(cantVar):
+            for j in range(cantRes):
+                self.linea.append(self.tabla.GetCellValue(i, j))
+            self.tablero.append(self.linea)
+        matriz = self.tablero
 
-class DosFasesUI(wx.Frame):
-    
+
+class DosFasesUI(wx.Frame):   
     def __init__ (self, *args, **kwargs):       
         super(DosFasesUI, self).__init__(*args, **kwargs)
         self.InitUI()
@@ -29,20 +52,16 @@ class DosFasesUI(wx.Frame):
         self.SetTitle('Metodo Dos Fases')
         self.SetSize((400,400))
         self.Titulo = wx.StaticText(self, -1, "Investigacion De Operaciones", (190,10))
-
         self.p1=wx.Panel(self)
-        self.p2=wx.Panel(self)
-        
+        self.p2=wx.Panel(self)       
         self.mainsz = wx.BoxSizer(wx.VERTICAL)
         self.p1sz = wx.BoxSizer(wx.VERTICAL)
         self.p2sz = wx.BoxSizer(wx.VERTICAL)
-
         self.st1=wx.StaticText(self.p1,-1,"Inserte El Numero De Variables",(0,10))
         self.txt1=wx.TextCtrl(self.p1,-1)
         self.st2=wx.StaticText(self.p2,-1,"Inserte El Numero De Restricciones")
         self.txt2=wx.TextCtrl(self.p2,-1)
         self.bt=wx.Button(self,-1,"Continuar")
-
         self.p1sz.Add(self.st1,1,wx.EXPAND|wx.ALL,10)
         self.p1sz.Add(self.txt1,1,wx.EXPAND|wx.ALL,10)
         self.p2sz.Add(self.st2,1,wx.EXPAND|wx.ALL,10)
@@ -50,12 +69,9 @@ class DosFasesUI(wx.Frame):
         self.mainsz.Add(self.p1,1,wx.EXPAND)
         self.mainsz.Add(self.p2,1,wx.EXPAND)
         self.mainsz.Add(self.bt,1,wx.ALIGN_CENTRE|wx.ALL,30)
-
         self.Bind(wx.EVT_BUTTON, self.onContinuar, self.bt)
-
         self.p1.SetSizer(self.p1sz)
         self.p2.SetSizer(self.p2sz)
-
         self.SetSizer(self.mainsz)
         self.Centre()
         self.Show(True)
@@ -63,9 +79,9 @@ class DosFasesUI(wx.Frame):
     def onContinuar(self, event):
         global cantVar 
         global cantRes
-        cantVar = self.txt1.GetValue()
-        cantRes = self.txt2.GetValue()
-        IntermedioUI(None)
+        cantVar = int(self.txt1.GetValue())
+        cantRes = int(self.txt2.GetValue())
+        RestriccionesUI(None)
         self.Destroy()
 
 class LagrangeUI(wx.Frame):
@@ -77,20 +93,16 @@ class LagrangeUI(wx.Frame):
         self.SetTitle('Metodo Dos Fases')
         self.SetSize((400,400))
         self.Titulo = wx.StaticText(self, -1, "Investigacion De Operaciones", (190,10))
-
         self.p1=wx.Panel(self)
-        self.p2=wx.Panel(self)
-        
+        self.p2=wx.Panel(self)        
         self.mainsz = wx.BoxSizer(wx.VERTICAL)
         self.p1sz = wx.BoxSizer(wx.VERTICAL)
         self.p2sz = wx.BoxSizer(wx.VERTICAL)
-
         self.st1=wx.StaticText(self.p1,-1,"Inserte El Numero De Variables",(0,10))
         self.txt1=wx.TextCtrl(self.p1,-1)
         self.st2=wx.StaticText(self.p2,-1,"Inserte El Numero De Restricciones")
         self.txt2=wx.TextCtrl(self.p2,-1)
         self.bt=wx.Button(self,-1,"Continuar")
-
         self.p1sz.Add(self.st1,1,wx.EXPAND|wx.ALL,10)
         self.p1sz.Add(self.txt1,1,wx.EXPAND|wx.ALL,10)
         self.p2sz.Add(self.st2,1,wx.EXPAND|wx.ALL,10)
@@ -98,18 +110,19 @@ class LagrangeUI(wx.Frame):
         self.mainsz.Add(self.p1,1,wx.EXPAND)
         self.mainsz.Add(self.p2,1,wx.EXPAND)
         self.mainsz.Add(self.bt,1,wx.ALIGN_CENTRE|wx.ALL,30)
-
         self.Bind(wx.EVT_BUTTON, self.onContinuar, self.bt)
-
         self.p1.SetSizer(self.p1sz)
         self.p2.SetSizer(self.p2sz)
-
         self.SetSizer(self.mainsz)
         self.Centre()
 
     def onContinuar(self, event):
+        global cantVar
+        global cantRes
         cantVar = self.txt1.GetValue()
         cantRes = self.txt2.GetValue()
+        RestriccionesUI(None)
+        self.Destroy()
 
 class ventanaInicio(wx.Frame):
     def __init__ (self, *args, **kwargs):
@@ -120,22 +133,17 @@ class ventanaInicio(wx.Frame):
         panel = wx.Panel(self)
         self.SetTitle('Investigacion de Operaciones')
         self.SetSize((600,600))
-        Titulo = wx.StaticText(panel, -1, "Investigacion De Operaciones", (190,10))
-        
+        Titulo = wx.StaticText(panel, -1, "Investigacion De Operaciones", (190,10))    
         sz=wx.BoxSizer(wx.VERTICAL)
         boton1 = wx.Button(panel,-1, u"Dos Fases")
         boton2 = wx.Button(panel,-1, u"Lagrange")
-        boton3 = wx.Button(panel,-1, u"Salir")
-
-        
+        boton3 = wx.Button(panel,-1, u"Salir")       
         sz.Add(boton1,1,wx.EXPAND|wx.ALL,40)
         sz.Add(boton2,1,wx.EXPAND|wx.ALL,40)
         sz.Add(boton3,1,wx.EXPAND|wx.ALL,60)
-
         self.Bind(wx.EVT_BUTTON, self.onClickButton1,boton1)
         self.Bind(wx.EVT_BUTTON, self.onClickButton2,boton2)
         self.Bind(wx.EVT_BUTTON, self.onQuit, boton3)
-
         self.SetSizer(sz)
         self.Centre()
         self.Show(True)
